661fbe3f13ce092ef562b876f1084f0305523bed
faizanfmgt <faizanmansoori1281@gmail.com>
2025-01-22 15:21:13 +0530

    File addeed

---
+++ NaturalNoSum.java
@@ -0,0 +1,11 @@
+public class NaturalNoSum {
+    public static void main (String[] args){
+
+        int sum = 0;
+        for(int i = 0; i<=100; i++){
+            sum +=i;
+
+        }
+        System.out.println("The sum of all positive number from 0 to 100 is: " + sum);
+    }
+}
