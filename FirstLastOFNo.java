e0a674f870ccd2fa41890210eccfe05668fd1750
faizanfmgt <faizanmansoori1281@gmail.com>
2025-01-22 15:42:49 +0530

    Added a new file

---
+++ FirstLastOFNo.java
@@ -0,0 +1,16 @@
+public class FirstLastOFNo {
+ public static void main(String[] args){
+    int number = 93996298;
+
+    int FirstNo = number;
+    int LastDigit = number % 10;
+
+
+    While(FirstNo >=10){
+        
+    }
+
+    System.out.println("The last digit of the number is: " + LastDigit);
+    
+ }   
+}
